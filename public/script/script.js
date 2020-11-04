a8886724e9a20ab3fa3c0c27c85ae437b51e2aff
Lythis <morgan60127@gmail.com>
2020-11-04 07:28:47 +0100

    rec

--- public/script/script.js
+++
@@ -1,7 +0,0 @@
-    var $document = $(document),
-    $element = $('#some-element'),
-    className = 'hasScrolled';
-
-$document.scroll(function() {
-  $element.toggleClass(scriptNav, $document.scrollTop() >= 10   );
-});
